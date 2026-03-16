63ad651f2821c98ac8a1bde5fca7bc2c3b70f96b
Taran Grewal <tarangrewal008@gmail.com>
2026-03-16 00:56:30 -0700

    Remove unnecessary root-level files

--- Meme-Bot/src/api/mediaApi.js
+++ Meme-Bot/src/api/mediaApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXEL_KEY = import.meta.env.VITE_PEXELS_KEY
 
-export async function fetchPhotos(query,page=1,per_page =20){
+export async function fetchPhotos(query,page=1,per_page =30){
 const res = await axios.get('https://api.unsplash.com/search/photos',
     {
         params:{query,page,per_page},
---
+++ Meme-Bot/src/redux/features/collectionSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import { toast, Zoom } from 'react-toastify';
+
+const initialState = {
+    items: JSON.parse(localStorage.getItem('collection')) || []
+}
+
+const collectionSlice = createSlice({
+    name: 'collection',
+    initialState,
+    reducers: {
+        addCollection: (state, action) => {
+            const alreadyExists = state.items.find(
+                item => item.id === action.payload.id
+            )
+            if (!alreadyExists) {
+                state.items.push(action.payload);
+                localStorage.setItem('collection', JSON.stringify(state.items))
+            }
+        },
+        removeCollection: (state, action) => {
+            console.log('removed');
+            
+            state.items = state.items.filter(
+                item => item.id !== action.payload
+            )
+            console.log(state.items);
+            
+            localStorage.setItem('collection', JSON.stringify(state.items))
+        },
+        clearCollection: (state) => {
+            state.items = []
+            localStorage.removeItem('collection')
+        },
+        addedToast: () => {
+            alert('Added to Collection ✅');
+        },
+        removeToast: () => {
+            alert('Removed from Collection');
+        }
+    }
+})
+
+
+export const {
+    addCollection,
+    removeCollection,
+    clearCollection,
+    addedToast,
+    removeToast,
+} = collectionSlice.actions;
+
+
+export default collectionSlice.reducer;
